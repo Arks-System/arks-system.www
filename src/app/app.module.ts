7e9e2495e84495836f0837c7b8b126f71867d244
SenpaiSilver <silvermesprims@gmail.com>
2017-03-30 23:14:51 +0200

    Adding pages for the update archive and AST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TilesComponent } from './tiles/tiles.component';
+import { ArchiveComponent } from './archive/archive.component';
+
+import { ArchiveService } from './archive.service';
+import { ToolComponent } from './tool/tool.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TilesComponent
+    TilesComponent,
+    ArchiveComponent,
+    ToolComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'archive',
+        component: ArchiveComponent
+      },
+      {
+        path: '',
+        component: TilesComponent,
+        pathMatch: 'full'
+      }
+      /*{
+        path: '',
+        redirectTo: '/tiles',
+        pathMatch: 'full'
+      }*/
+    ]),
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    ArchiveService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/tiles/tiles.component.ts
+++ src/app/tiles/tiles.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tiles.component.css']
 })
 export class TilesComponent implements OnInit {
-  links: {name: string, href: string}[] = [
+  links = [
     {name: 'Arks-System Tool', href: 'http://tool.arks-system.eu'},
     {name: 'Steam Community', href: 'https://steamcommunity.com/groups/Arks-System'},
     {name: 'Recrutement (Steam)', href: 'https://steamcommunity.com/groups/Arks-System/discussions/0/540735426139834762/'},
     {name: 'Recrutement (Formulaire)', href: 'https://goo.gl/vqlBNB'},
-    {name: 'Archives', href: 'http://arks-system.eu/update/'}
+    {name: 'Archives', routerLink: '/archive'}
   ];
   public constructor() {
   }
