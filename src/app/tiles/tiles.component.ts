9d29dc5adbe431658d8b940b305bd6bbea1e4c5a
SenpaiSilver <silvermesprims@gmail.com>
2017-03-30 02:25:13 +0200

    Updated the code to use tiles

--- src/app/tiles/tiles.component.ts
+++ src/app/tiles/tiles.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import 'links';
 
 @Component({
   selector: 'app-tiles',
@@ -7,10 +6,17 @@ import 'links';
   styleUrls: ['./tiles.component.css']
 })
 export class TilesComponent implements OnInit {
-  links: Links = new Links;
-  constructor() { }
+  links: {name: string, href: string}[] = [
+    {name: 'Arks-System Tool', href: 'http://tool.arks-system.eu'},
+    {name: 'Steam Community', href: 'https://steamcommunity.com/groups/Arks-System'},
+    {name: 'Recrutement (Steam)', href: 'https://steamcommunity.com/groups/Arks-System/discussions/0/540735426139834762/'},
+    {name: 'Recrutement (Formulaire)', href: 'https://goo.gl/vqlBNB'},
+    {name: 'Archives', href: 'http://arks-system.eu/update/'}
+  ];
+  public constructor() {
+  }
 
-  ngOnInit() {
+  public ngOnInit() {
   }
 
 }
