c8d371a65e30fe782929fe5b11242b8492ddecec
SenpaiSilver <silvermesprims@gmail.com>
2021-04-18 00:47:57 +0200

    Fuck Gulp and fuck everything it stands for because nodejs is a mistake

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
 const autoprefixer = require('gulp-autoprefixer');
-const csso         = require('gulp-csso');
+// const csso         = require('gulp-csso');
 const del          = require('del');
 const gulp         = require('gulp');
 const util         = require('gulp-util');
-const runSequence  = require('run-sequence');
+// const runSequence  = require('run-sequence');
 const uglify       = require('gulp-uglify');
 const concat       = require('gulp-concat');
-const zip          = require('gulp-zip');
+// const zip          = require('gulp-zip');
 const sass         = require('gulp-sass');
 const sourcemaps   = require('gulp-sourcemaps');
 const plumber      = require('gulp-plumber');
-const os           = require('os');
+// const os           = require('os');
 
-var pjson          = require('./package.json');
-var execSync       = require('child_process').execSync;
+// var pjson          = require('./package.json');
+// var execSync       = require('child_process').execSync;
 
 sass.compiler = require('node-sass');
 
@@ -36,9 +36,50 @@ const AUTOPREFIXER_BROWSERS = [
     'bb >= 10'
 ];
 
-// gulp.task('default', ['mini']);
+gulp.task('clean', gulp.series(() => del([
+    './assets/dist/css/',
+    './assets/dist/js/',
+    './assets/dist/resources/'
+])));
 
-gulp.task('default', ['clean'], function () {
+gulp.task('scripts', gulp.series(() => {
+    return gulp.src([
+            "./assets/js/main.js",
+        ])
+        .pipe(plumber())
+        .pipe(config.production ? util.noop() : sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(config.production ? uglify() : util.noop())
+        .pipe(config.production ? util.noop() : sourcemaps.write())
+        .pipe(gulp.dest('./assets/dist/js'));
+}));
+
+gulp.task('sass', gulp.series(() => {
+    let output = {};
+
+    if (config.production)
+        output = {outputStyle: 'compressed'};
+
+    return gulp.src('./assets/css/**/*.scss')
+        .pipe(config.production ? util.noop() : sourcemaps.init())
+        .pipe(sass(output).on('error', sass.logError))
+        .pipe(config.production ? util.noop() : sourcemaps.write())
+        .pipe(gulp.dest('./assets/dist/css'));
+}));
+
+gulp.task('fonts', gulp.series(() => {
+    return gulp.src('./assets/fonts/**/*')
+        .pipe(gulp.dest('./assets/dist/resources/fonts'));
+}));
+
+gulp.task('resources', gulp.series(() => {
+    return gulp.src('./assets/img/**/*')
+        .pipe(gulp.dest('./assets/dist/resources/'));
+}));
+
+gulp.task('static', gulp.series(["resources", "fonts"]));
+
+gulp.task('default', gulp.series('clean', (done) => {
     if (!config.production) {
         console.warn("")
         console.warn(" ██╗    ██╗ █████╗ ██████╗ ███╗   ██╗██╗███╗   ██╗ ██████╗");
@@ -50,45 +91,16 @@ gulp.task('default', ['clean'], function () {
         console.warn("\nThis is a dev build only, run 'gulp prod' for a production build.\n\n");
     }
 
-    runSequence(
-        'sass',
-        'scripts'
-    );
-});
+    done();
+}, "static", "sass", "scripts"));
 
-gulp.task('prod', ['clean'], function () {
+gulp.task('prod', gulp.series('clean', () => {
     config.production = true;
-    runSequence('default');
-});
+    gulp.series('default');
+}, "default"));
 
-gulp.task('clean', () => del(['./assets/dist/']));
-
-gulp.task('scripts', function() {
-    return gulp.src([
-            "./assets/js/main.js",
-        ])
-        .pipe(plumber())
-        .pipe(config.production ? util.noop() : sourcemaps.init())
-        .pipe(concat('main.js'))
-        .pipe(config.production ? uglify() : util.noop())
-        .pipe(config.production ? util.noop() : sourcemaps.write())
-        .pipe(gulp.dest('./assets/dist/js'));
-});
-
-gulp.task('sass', function () {
-    let output = {};
-
-    if (config.production)
-        output = {outputStyle: 'compressed'};
-
-    return gulp.src('./assets/css/**/*.scss')
-        .pipe(config.production ? util.noop() : sourcemaps.init())
-        .pipe(sass(output).on('error', sass.logError))
-        .pipe(config.production ? util.noop() : sourcemaps.write())
-        .pipe(gulp.dest('./assets/dist/css'));
-});
-
-gulp.task('watch', function(){
-    gulp.watch('./assets/js/**/*.js', ['scripts']);
-    gulp.watch('./assets/css/**/*.scss', ['sass']);
-});
+gulp.task('watch', gulp.series((done) => {
+    gulp.watch('./assets/js/**/*.js', gulp.series('scripts'));
+    gulp.watch('./assets/css/**/*.scss', gulp.series('sass'));
+    done();
+}));
